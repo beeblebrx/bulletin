0ffe4e06a32bfd222a6a2535f37a51d40bc46698
kimmo <beeblebrx@tux.fi>
2014-03-13 23:24:38 +0200

    Fixed jshint errors on server.js, Gruntfile and karma configs.

--- server.js
+++ server.js
@@ -16,21 +16,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./lib/config/config');
 
 // Connect to database
-var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Bootstrap models
 var modelsPath = path.join(__dirname, 'lib/models');
 fs.readdirSync(modelsPath).forEach(function (file) {
-  if (/(.*)\.(js$|coffee$)/.test(file)) {
-    require(modelsPath + '/' + file);
-  }
+    if (/(.*)\.(js$|coffee$)/.test(file)) {
+        require(modelsPath + '/' + file);
+    }
 });
 
 // Populate empty DB with sample data
 require('./lib/config/dummydata');
 
 // Passport Configuration
-var passport = require('./lib/config/passport');
+require('./lib/config/passport');
 
 var app = express();
 
@@ -42,7 +42,7 @@ require('./lib/routes')(app);
 
 // Start server
 app.listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+    console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
 });
 
 // Expose app
