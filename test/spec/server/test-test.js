506b9bd9b7be94b0acd3200c5454a32f0542d9a1
kimmo <beeblebrx@tux.fi>
2014-03-13 23:28:23 +0200

    Removed a test that was there only to test Mocha and Chai.

--- test/spec/server/test-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* jshint -W079 */
-var expect = require('chai').expect;
-
-describe('Testi', function(){
-    it('should not fail', function() {
-        expect(false).to.equal(false);
-      });
-  });
