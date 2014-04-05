e2b5671d2f0895167ac71387b7b301bfed6561ff
kimmo <beeblebrx@tux.fi>
2014-04-05 22:06:49 +0300

    Configured Karma to use Firefox. Added configuration for continuous integration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -381,6 +381,10 @@ module.exports = function (grunt) {
             unit: {
                 configFile: 'karma.conf.js',
                 background: true
+            },
+            continuous: {
+                singleRun: true,
+                browsers: ['PhantomJS']
             }
         },
         mochaTest: {
